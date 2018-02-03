881e848b4c8bbadd06d872e0291d55077760ce75
Arian Valles <arian.valles@outlook.com>
2018-02-03 01:17:28 -0300

    add components imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,30 +5,43 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { AboutPage } from '../pages/about/about';
+import { MovieCardComponent } from '../components/movie-card/movie-card';
+import { DataMoviePage } from '../pages/data-movie/data-movie';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { ConnectToApiProvider } from '../providers/connect-to-api/connect-to-api';
+import { HttpClientModule } from '@angular/common/http/';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    AboutPage,
+    MovieCardComponent,
+    DataMoviePage
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    HttpClientModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    AboutPage,
+    MovieCardComponent,
+    DataMoviePage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ConnectToApiProvider
   ]
 })
 export class AppModule {}
