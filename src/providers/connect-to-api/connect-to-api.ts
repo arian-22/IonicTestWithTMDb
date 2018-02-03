606376b693fad5509b9b195ea12abcfce23389db
Arian Valles <arian.valles@outlook.com>
2018-02-03 01:17:05 -0300

    function to get movies from tmdb

--- src/providers/connect-to-api/connect-to-api.ts
+++ src/providers/connect-to-api/connect-to-api.ts
@@ -9,9 +9,21 @@ import { Injectable } from '@angular/core';
 */
 @Injectable()
 export class ConnectToApiProvider {
+  baseURL: string = "https://api.themoviedb.org/3";
+  apiKey: string = "66ae687f31e3066ab23a1b7128278d17";
+  parameterKey: string = "?api_key=" + this.apiKey;
+
 
   constructor(public http: HttpClient) {
     console.log('Hello ConnectToApiProvider Provider');
+  }
+
+  loadMovieDiscover(page:number = 1) {
+    return this.http.get(this.baseURL + "/discover/movie" + this.parameterKey + "&sort_by=popularity.desc&page=" + page);
+  }
+
+  getMovieInfo() {
+
   }
 
 }
